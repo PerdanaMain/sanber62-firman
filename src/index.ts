248de0573e8b6541e45b2c46879c5ab6307a3a7c
Perdana Main <firman.fp123@gmail.com>
2024-12-30 21:58:39 +0700

    fix: swagger docs

--- src/index.ts
+++ src/index.ts
@@ -28,15 +28,13 @@ async function main() {
 
   app.use(router);
 
-  const css = fs.readFileSync(
-    path.resolve(__dirname, "../node_modules/swagger-ui-dist/swagger-ui.css"),
-    "utf-8"
-  );
+  const CSS_URL =
+    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
   app.use(
     "/docs",
     swaggerUi.serve,
     swaggerUi.setup(swaggerOutput, {
-      customCss: css,
+      customCss: CSS_URL,
     })
   );
 
